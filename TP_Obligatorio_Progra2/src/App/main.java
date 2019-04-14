729d64e9d9079d899297b04ccd74b2cedec61189
rapaportmatias <rapaport.matias@gmail.com>
2019-04-14 17:17:12 -0300

    Crea la clase FILE que permite levantar los archivos y guardarlos en un diccionario. Realiza la tarea siguiendo estos pasos; 1) Crea una diccionario 2) Crea un path relativo para encontrar los arhcivos en la computadora ( es distinto por computadora) 3) Lee un archivo de configuracion donde hay una lista de archvios csv a leer 4) Comeinza a leer los archivos segun la lista  5) guarda cada eleemntos del archivo csv con un formato predetermiandao. Formato A para el enuncia 1a, Formato B para el enunciado 1b, ect. Hay que revisar el diccionario y conjunto poruqe la implementacion realizada era para numeros y los arhcivos se trabaja con strings, hice los cambios minimos para que funcionaran pero me parece que el conjunto no fucniona, porque esta permitiendo elementos repetidos. Otra cosa es que no se puede por el momento levantar todos los archivos juntos porque el programa rompe, se supera los 100 elementos en el array, no se si el problema es el tema de la clase conjunto o otro.

---
+++ TP_Obligatorio_Progra2/src/App/main.java
@@ -0,0 +1,14 @@
+package App;
+
+import MecanicasDeAlmacenamiento.DiccionarioMultipleDinamico;
+import Metodos.File;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DiccionarioMultipleDinamico dicionario = File.CargarDiccionario("B");
+		dicionario.Imprimir();
+	}
+
+}
---
+++ TP_Obligatorio_Progra2/src/Metodos/File.java
@@ -0,0 +1,109 @@
+package Metodos;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import MecanicasDeAlmacenamiento.DiccionarioMultipleDinamico;
+
+public class File {
+	
+	private static String NOMBRE_ARCHIVO_DE_CONFIGURACION = "Config.txt";
+	private static String PATH_NOMBRE_CARPETA_DE_ARCHIVOS = "\\src\\Archivos\\";
+	
+	private static void FormatoB(DiccionarioMultipleDinamico diccionario, String[] values, String archivoNombre) {
+		if(values.length == 4)	
+			diccionario.Agregar(archivoNombre, values[1]);
+	}
+	
+	private static void FormatoA(DiccionarioMultipleDinamico diccionario, String[] values, String archivoNombre) {
+		diccionario.Agregar(archivoNombre, values[1]);
+	}
+	
+	private static void AgregarAlDiccionarioConFormato(DiccionarioMultipleDinamico diccionario, String[] values, String formato, String archivoNombre) {
+		
+		switch(formato) {
+			case "A":
+				FormatoA(diccionario, values, archivoNombre);
+				break;
+			case "B":
+				FormatoB(diccionario, values, archivoNombre);
+				break;
+			default:
+				System.out.print("Formato no valiod, intentalo de nuevo.");
+				break;
+		}
+	}
+		
+	private static String ObtenerPathDeCarpetaDeArchivos() {
+		return System.getProperty("user.dir") + PATH_NOMBRE_CARPETA_DE_ARCHIVOS;
+	}
+	
+	
+	private static ArrayList<String> ObtenerListaDeArchivos(){
+		ArrayList<String> ListaConNombresDeArchivos = new ArrayList<String>();
+		FileReader fr;
+		BufferedReader br;
+		String path = ObtenerPathDeCarpetaDeArchivos();
+		String line = null;
+
+		try
+		{
+			fr = new FileReader(path + NOMBRE_ARCHIVO_DE_CONFIGURACION);
+			br = new BufferedReader(fr);
+			
+			while ((line = br.readLine()) != null) {
+				ListaConNombresDeArchivos.add(line.toString());
+			}
+		}
+		catch (IOException e)
+		{
+			System.out.print("El archivo de configuracion con al lista de archivos csv no se encontro");
+		}
+		
+		return ListaConNombresDeArchivos;
+	}
+	
+	
+	public static void LeerArchvioCSV(DiccionarioMultipleDinamico diccionario, String archivo, String formato) {
+		
+		BufferedReader br = null;
+		FileReader fr = null;
+		String line = null;
+		String[] values = null;
+		String path = ObtenerPathDeCarpetaDeArchivos();
+		
+		try {
+			fr = new FileReader(path + archivo);
+			br = new BufferedReader(fr);
+			
+			while ((line = br.readLine()) != null) {
+		        values = line.split(";");
+		        AgregarAlDiccionarioConFormato(diccionario, values, formato, archivo);	        
+		    }
+			
+			br.close();
+			
+		} catch (IOException e) {
+			System.out.print("No se encontro el archivo : " + archivo + "\n");
+		}
+	}
+	
+	
+	public static DiccionarioMultipleDinamico CargarDiccionario(String formato)  {
+		
+		DiccionarioMultipleDinamico diccionario = new DiccionarioMultipleDinamico();
+		diccionario.InicializarDiccionario();
+		
+		ArrayList<String> listaDeArchivos;
+		String archivo;
+		listaDeArchivos = ObtenerListaDeArchivos();
+		for(int i = 0; i < listaDeArchivos.size();i++) {
+			archivo =  listaDeArchivos.get(i);
+			LeerArchvioCSV(diccionario , archivo, formato);
+		}
+		
+		return diccionario;
+	}
+	
+}
